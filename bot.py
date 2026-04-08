9670e61c981bfcc2146c135f1077a6ef9c96d58a
DlaskerLerto <168008515+DlaskerLerto@users.noreply.github.com>
2026-04-09 01:53:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -721,4 +721,4 @@ def webhook():
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
